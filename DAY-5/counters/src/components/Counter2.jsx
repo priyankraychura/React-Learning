5a4bc01c12ea85d4c83cfc1076a67d48a0456497
priyankraychura <priyankraychura@gmail.com>
2025-01-02 18:48:23 +0530

    counter with props

--- DAY-5/counters/src/components/Counter2.jsx
+++ DAY-5/counters/src/components/Counter2.jsx
@@ -5,26 +5,38 @@ import Counter3 from "./Counter3";
 
 function Counter2() {
     const [count, setCount] = useState(0)
+    const [message, setMessage] = useState("");
 
     let handleIncrement = () => {
-        setCount(count + 1)
+        if(count >= 10) {
+            setMessage("Maximum Limit Reached")
+            return
+        } 
+        setCount(count + 1);
+        setMessage("");
     }
 
     let handleDecrement = () => {
+        if(count <= 0) {
+            setMessage("Minimum Limit Reached")
+            return
+        }
         setCount(count - 1)
+        setMessage("");
     }
 
     return (
         <>
-        <div className="counters">
-                <Counter1 num={count}/>
-            <div className="counter-card center-card">
-                <button onClick={handleIncrement}>Increment</button>
-                <h1>{count}</h1>
-                <button onClick={handleDecrement}>Decrement</button>
+            <div className="counters">
+                    <Counter1 num={count}/>
+                <div className="counter-card center-card">
+                    <button onClick={handleIncrement}>Increment</button>
+                    <h1>{count}</h1>
+                    <button onClick={handleDecrement}>Decrement</button>
+                    <div className="msg"><h3>{message}</h3></div>
+                </div>
+                    <Counter3 num={count}/>
             </div>
-                <Counter3 num={count}/>
-        </div>
         </>
     )
 }
