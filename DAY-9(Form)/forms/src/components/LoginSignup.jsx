654271c4c57eea5ba44b0761dd078b50ef0b1360
priyankraychura <priyankraychura@gmail.com>
2025-01-10 18:51:46 +0530

    field validatuin added

--- DAY-9(Form)/forms/src/components/LoginSignup.jsx
+++ DAY-9(Form)/forms/src/components/LoginSignup.jsx
@@ -12,6 +12,11 @@ function LoginSignup() {
         password: ""
     });
     const [users, setUsers] = useState([]);
+    const [isValid, setIsValid] = useState({
+        username: true,
+        email: true,
+        password: true
+    });
 
     const handleOnChange = (e) => {
         setFormData({...formData, [e.target.name] : e.target.value})
@@ -20,9 +25,50 @@ function LoginSignup() {
     const handleSubmit = (e) => {
         e.preventDefault();
         setUsers([...users, formData])
+        setFormData({
+            username: "",
+            email: "",
+            password: ""
+        })
     }
 
-    console.log(users);
+    const handleBlur = (e) => {
+       let username = /^[0-9A-Za-z]{6,16}$/;
+       let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+       let passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+
+       
+       console.log(username.test(formData.username));
+       console.log(emailRegex.test(formData.email));
+       console.log(passwordRegex.test(formData.password));
+       
+    //    if(e.target.value.length < 3) {
+    //     setIsValid({...isValid, [e.target.name]: false})
+    //    }
+
+        if(e.target.name == "username") {
+            console.log("username");
+            if(!username.test(formData.username)) {
+                setIsValid({...isValid, [e.target.name]: false})
+            }
+        } else if (e.target.name == "email") {
+            console.log("email");
+            if(!emailRegex.test(formData.email)) {
+                setIsValid({...isValid, [e.target.name]: false})
+            }
+        } else if (e.target.name == "password") {
+            console.log("password");
+            if(!passwordRegex.test(formData.password)) {
+                setIsValid({...isValid, [e.target.name]: false})
+            }
+        }
+    }
+
+    const handleFocus = (e) => {
+        setIsValid({...isValid, [e.target.name]: true})
+    }
+    // console.log(isValid);
+
     
 
     return (
@@ -32,7 +78,7 @@ function LoginSignup() {
             <button onClick={() => setShowLogin(true)}>Signup</button>
         </header>
         {
-            showLogin ? <Signup handleOnChange={handleOnChange} handleSubmit={handleSubmit}/> : <Login users={users}/> 
+            showLogin ? <Signup formData={formData} handleOnChange={handleOnChange} handleSubmit={handleSubmit} handleBlur={handleBlur} handleFocus={handleFocus} isValid={isValid}/> : <Login users={users}/> 
         }
     </>
    )
--- DAY-9(Form)/forms/src/components/Signup.jsx
+++ DAY-9(Form)/forms/src/components/Signup.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import '../App.css';
 
 
-function Signup({handleOnChange, handleSubmit}) {    
+function Signup({formData, handleOnChange, handleSubmit, handleBlur, handleFocus, isValid}) {    
     return (
         <>
             <div className="main">
@@ -10,11 +10,11 @@ function Signup({handleOnChange, handleSubmit}) {
                     <h1>Signup</h1>
                     <form action="" onSubmit={handleSubmit}>
                         <label htmlFor="">Username</label>
-                        <input type="text" placeholder="username" name="username" onChange={handleOnChange}/>
+                        <input type="text" className={!isValid.username ? 'invalid' : ''} onBlur={handleBlur} onFocus={handleFocus} placeholder="username" value={formData.username} name="username" onChange={handleOnChange}/>
                         <label htmlFor="">Email</label>
-                        <input type="text" placeholder="email" name="email" onChange={handleOnChange} />
+                        <input type="text" className={!isValid.email ? 'invalid' : 'valid'} onBlur={handleBlur} onFocus={handleFocus} placeholder="email" value={formData.email} name="email" onChange={handleOnChange} />
                         <label htmlFor="">Password</label>
-                        <input type="text" placeholder="password" name="password" onChange={handleOnChange} />
+                        <input type="text" className={!isValid.password ? 'invalid' : 'valid'} onBlur={handleBlur} onFocus={handleFocus} placeholder="password" value={formData.password} name="password" onChange={handleOnChange} />
                         <input type='submit' className='form-btn' value="Register" />
                     </form>
                 </div>
