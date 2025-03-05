87db01995197755d85d52cefa4e83d73fc4e2a8b
priyankraychura <priyankraychura@gmail.com>
2025-03-05 18:08:12 +0530

    crud completed

--- DAY-27(FirebaseAuthentication)/src/components/Dashboard.jsx
+++ DAY-27(FirebaseAuthentication)/src/components/Dashboard.jsx
@@ -3,13 +3,16 @@ import { use, useEffect, useState } from 'react'
 import { auth, db } from '../../firebaseConfig';
 import { useNavigate } from 'react-router';
 import toast from 'react-hot-toast';
-import { doc, getDoc } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, updateDoc } from 'firebase/firestore';
 import { PuffLoader } from 'react-spinners';
 
 export default function Dashboard() {
   const [uid, setUid] = useState(null);
+  const [editIndex, setEditIndex] = useState(null);
   const [loading, setLoading] = useState(true);
   const [userData, setUserData] = useState("Loading...");
+  const [record, setRecord] = useState({ task: '', priority: '' });
+  const [localData, setLocalRecord] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -25,6 +28,7 @@ export default function Dashboard() {
   useEffect(() => {
     if (uid) {
       fetchUser();
+      fetchRecords();
     }
   }, [uid])
 
@@ -46,18 +50,110 @@ export default function Dashboard() {
       })
   }
 
+  const fetchRecords = async () => {
+    await getDocs(collection(db, 'tasks'))
+      .then((res) => {
+        let data = res.docs.map((el, i) => {
+          return { id: el.id, ...el.data() }
+        })
+        setLocalRecord(data);
+      })
+  }
+
+  const deleteTask = async (id) => {    
+    await deleteDoc(doc(db, 'tasks', id))
+      .then((res) => {
+        console.log(res);
+        
+        setLocalRecord(localData.filter((el) => el.id !== id));
+      })
+  }
+
+  const updateTask = (id) => {
+    let singleTask = localData.find((el) => el.id === id);
+    setEditIndex(id)
+    setRecord(singleTask);
+  }
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setRecord({ ...record, [name]: value });
+  }
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+
+    if(editIndex == null) {
+      await addDoc(collection(db, 'tasks'), { userId: uid, ...record })
+      .then((res) => {
+        setLocalRecord([...localData, { id: res.id, ...record }]);
+      })
+    } else {
+      await updateDoc(doc(db, 'tasks', editIndex), record)
+      .then((res) => {
+        fetchRecords();
+      })
+    }
+
+    setRecord({ task: '', priority: '' });
+    setEditIndex(null)
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
-      {loading ? 
-        <PuffLoader 
+      {loading ?
+        <PuffLoader
           color='#FFF'
           className='loader'
-        />:
+        /> :
         <div>
           <h2>Welcome, {userData.name}</h2>
-          <h2>{userData.email}</h2>
-          <button onClick={handleLogout}>Logout</button>
+          <button onClick={handleLogout}>Logout</button>  <br /><br />
+          <div className='main'>
+            <div className="task-list">
+              <h2>Tasks</h2>
+              <table>
+                <thead>
+                <tr>
+                  <th>No.</th>
+                  <th>Task</th>
+                  <th>Priority</th>
+                  <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                {localData.map((el, i) => {
+                  return (
+                  <tr key={el.id}>
+                    <td>{i + 1}</td>
+                    <td>{el.task}</td>
+                    <td>{el.priority}</td>
+                    <td className='action'>
+                      <i onClick={() => deleteTask(el.id)} className="fa-solid fa-xmark"></i>
+                      <i onClick={() => updateTask(el.id)} className="fa-solid fa-pencil"></i>
+                    </td>
+                  </tr>
+                  )
+                })}
+                </tbody>
+
+              </table>
+            </div>
+            <form action="" onSubmit={handleOnSubmit}>
+              <h2>Add Task</h2>
+              <label htmlFor="">Task</label>
+              <input type="text" name='task' value={record.task} onChange={handleOnChange} />
+              <label htmlFor="">Priority</label>
+              <select name="priority" value={record.priority} id="" onChange={handleOnChange}>
+                <option hidden>Select Priority</option>
+                <option value="High">High</option>
+                <option value="Medium">Medium</option>
+                <option value="Low">Low</option>
+              </select>
+              <button className='btn'>{editIndex == null ? "Add Task" : "Update Task"}</button>
+            </form>
+          </div>
         </div>
       }
     </div>
